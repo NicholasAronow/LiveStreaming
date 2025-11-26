eb349df66063210b2b094b84a1ef5eabfd8067ee
Aryan Farhang <fparyan28@gmail.com>
2025-11-26 11:21:38 -0800

    pushing and deploying to porter #1

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { broadcastStreamStatus, formatStreamStatus } from './webview';
 
 const PACKAGE_NAME = process.env.PACKAGE_NAME ?? (() => { throw new Error('PACKAGE_NAME is not set in .env file'); })();
 const MENTRAOS_API_KEY = process.env.MENTRAOS_API_KEY ?? (() => { throw new Error('MENTRAOS_API_KEY is not set in .env file'); })();
-const PORT = parseInt(process.env.PORT || '3000');
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 80;
 
 class StreamerApp extends AppServer {
   /** Map to store active user sessions */
